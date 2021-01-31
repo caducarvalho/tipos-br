73d2e2759c719203d729843be07936a464bcdab9
Cadu Carvalho <cadu.carvalho@me.com>
2021-01-31 18:46:38 -0300

    Assign credits to font authors

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -14,7 +14,31 @@ const Footer = () => (
       , organizador do
       {' '}
       <a href="https://facebook.com/groups/tipocracia" target="_blank" rel="noopener noreferrer">Tipocracia</a>
-      . Página desenvolvida por
+      .
+      <br />
+      As famílias tipográficas utilizadas são:
+      {' '}
+      <a href="https://fraunces.undercase.xyz" target="_blank" rel="noopener noreferrer">Fraunces</a>
+      , de
+      {' '}
+      <a href="https://twitter.com/phaedracharles" target="_blank" rel="noopener noreferrer">Phaedra Charles</a>
+      {' '}
+      e
+      {' '}
+      <a href="https://flaviazim.com/" target="_blank" rel="noopener noreferrer">Flavia Zimbardi</a>
+      ; e
+      {' '}
+      <a href="http://www.justintype.com/tomorrow" target="_blank" rel="noopener noreferrer">Tomorrow</a>
+      , de
+      {' '}
+      <a href="https://twitter.com/MonicaRizzolli" target="_blank" rel="noopener noreferrer">Monica Rizzolli</a>
+      {' '}
+      e
+      {' '}
+      <a href="https://twitter.com/tonydemarco" target="_blank" rel="noopener noreferrer">Tony de Marco</a>
+      .
+      <br />
+      Página desenvolvida por
       {' '}
       <a href="https://linktr.ee/caducarvalho" target="_blank" rel="noopener noreferrer">Cadu Carvalho</a>
       .
